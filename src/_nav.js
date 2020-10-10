93f2d69a9ca03ce4c326ed251d161a777fb0d6d3
DanielDO93 <61757109+DanielDO93@users.noreply.github.com>
2020-10-10 00:12:26 -0500

    Update _nav.js

--- src/_nav.js
+++ src/_nav.js
@@ -5,80 +5,230 @@ export default {
       name: "Inicio",
       class: "text-center",
     },
+
     {
       name: "1. Introducción al Centro de Contacto",
       icon: "icon-tag",
-      title: true,
+
+      children: [
+        {
+          name: "Introducción > P1",
+          url: "/temarioViews/TemaUnoView",
+          icon: "icon-tag",
+        },
+        {
+          name: "Introducción > P2",
+          url: "/temarioViews/TemaDosView",
+          icon: "icon-tag",
+        },
+
+        {
+          name: "Línea del tiempo          ",
+          url: "/temarioViews/TemaTresView",
+          icon: "icon-tag",
+        },
+        {
+          name: "Objetivos     ",
+          url: "/temarioViews/TemaCuatroView",
+          icon: "icon-tag",
+        },
+        {
+          name: "  Tipos de Servicio   > P1     ",
+          url: "/temarioViews/TemaCincoView",
+          icon: "icon-tag",
+        },
+        {
+          name: "Tipos de Servicio > P2",
+          url: "/temarioViews/TemaSeisView",
+          icon: "icon-tag",
+        },
+      ],
+    },
+    {
+      name: "1.1 ¿Qué es un Call Center?",
+      icon: "icon-tag",
+
+      children: [
+        {
+          name: "Call Center",
+          url: "/temarioViews/TemaSieteView",
+          icon: "icon-tag",
+        },
+        {
+          name: "Funcionamiento",
+          url: "/temarioViews/TemaOchoView",
+          icon: "icon-tag",
+        },
+      ],
     },
 
     {
-      name: "1.1  ¿Qué es un callcenter?",
+      name: "1.2 ¿Qué es un Centro de Contacto?",
       icon: "icon-tag",
-      title: true,
+      children: [
+        {
+          name: "Centro de Contacto          ",
+          url: "/temarioViews/TemaNueveView",
+          icon: "icon-tag",
+        },
+        {
+          name: "Funcionamiento",
+          url: "/temarioViews/TemaDiezView",
+          icon: "icon-tag",
+        },
+      ],
     },
     {
-      name: "1.2  ¿Qué es un centro de Contacto?      ",
+      name: "1.3 Diferencias entre Call Center y Centro de contacto      ",
       icon: "icon-tag",
-      title: true,
+      children: [
+        {
+          name: "Diferencias   > P1     ",
+          url: "/temarioViews/TemaOnceView",
+          icon: "icon-tag",
+        },
+        {
+          name: "Diferencias > P2",
+          url: "/temarioViews/TemaDoceView",
+          icon: "icon-tag",
+        },
+        {
+          name: "Diferencias > P2",
+          url: "/temarioViews/TemaTreceView",
+          icon: "icon-tag",
+        },
+        {
+          name: "Canales > P1  ",
+          url: "/temarioViews/TemaCatorceView",
+          icon: "icon-tag",
+        },
+        {
+          name: "Canales > P2",
+          url: "/temarioViews/TemaQuiceView",
+          icon: "icon-tag",
+        },
+        {
+          name: "Canales > P3",
+          url: "/temarioViews/TemaDieciseisView",
+          icon: "icon-tag",
+        },
+        {
+          name: "Canales > P4",
+          url: "/temarioViews/TemaDiecisieteView",
+          icon: "icon-tag",
+        },
+      ],
     },
     {
-      name: "1.3  Diferencias entre Callcenter y centro de contacto.      ",
+      name: "1.4 ¿Qué es lo que hace un Centro de Contacto?      ",
       icon: "icon-tag",
-      title: true,
-    },
-    {
-      name: "1.4  ¿Qué es lo que hace un Centro de Contacto?      ",
-      icon: "icon-tag",
-      title: true,
-    },
-    {
-      name: "1.5  Funciones de un Agente Telefónico",
-      icon: "icon-tag",
-      title: true,
-    },
-    {
-      name: "1.6  Servicios de un Centro de Contacto      ",
-      icon: "icon-tag",
-      title: true,
-    },
-    {
-      name: "1.7 Líneas de Negocio       ",
-      icon: "icon-tag",
-      title: true,
-    },
-    {
-      name: "1.7.1  Consultoría      ",
-      icon: "icon-tag",
-      title: true,
-    },
-    {
-      name: "1.7.2  CoSourcing      ",
-      icon: "icon-tag",
-      title: true,
-    },
-    {
-      name: "1.7.3  Outsourcing      ",
-      icon: "icon-tag",
-      title: true,
-    },
-    {
-      name: "1.7.4  Capacitación      ",
-      icon: "icon-tag",
-      title: true,
-    },
-    {
-      name: "1.7.5  Calidad      ",
-      icon: "icon-tag",
-      title: true,
-    },
-    {
-      name: "1.7.6  Business Inteligencie      ",
-      icon: "icon-tag",
-      title: true,
+
+      children: [
+        {
+          name: "CC",
+          url: "/temarioViews/TemadiecinueveView",
+          icon: "icon-tag",
+        },
+      ],
     },
 
-   
+    {
+      name: "1.5 ¿Funciones de un Agente Telefónico?",
+      icon: "icon-tag",
 
+      children: [
+        {
+          name: "Funciones > P1",
+          url: "/temarioViews/TemaVeinteView",
+          icon: "icon-tag",
+        },
+        {
+          name: "Funciones > P2",
+          url: "/temarioViews/TemaVeintiunoView",
+          icon: "icon-tag",
+        },
+        {
+          name: "Funciones > P3",
+          url: "/temarioViews/TemaVeintidosView",
+          icon: "icon-tag",
+        },
+      ],
+    },
+
+    {
+      name: "1.6 Servicios en un Centro de Contacto      ",
+      icon: "icon-tag",
+
+      children: [
+        {
+          name: "Servicios",
+          url: "/temarioViews/TemaVeintitresView",
+          icon: "icon-tag",
+        },
+        {
+          name: "Para finalizar          ",
+          url: "/temarioViews/TemaVeinticuatroView",
+          icon: "icon-tag",
+        },
+      ],
+    },
+    {
+      name: "1.7 Líneas de Negocio      ",
+      icon: "icon-tag",
+
+      children: [
+        {
+          name: "¿Qué es? ",
+          url: "/temarioViews/TemaVeinticincoView",
+          icon: "icon-tag",
+        },
+        {
+          name: "Consultoría ",
+          url: "/temarioViews/TemaVeintiseisView",
+          icon: "icon-tag",
+        },
+        {
+          name: "Outsourcing          ",
+          url: "/temarioViews/TemaVeintisieteView",
+          icon: "icon-tag",
+        },
+        {
+          name: "TIPOS DE OUTSOURCING ",
+          url: "/temarioViews/TemaVeintiochoView",
+          icon: "icon-tag",
+        },
+        {
+          name: "ETAPAS DEL PROCESO DE OUTSOURCING > P1 ",
+          url: "/temarioViews/TemaVeintinueveView",
+          icon: "icon-tag",
+        },
+        {
+          name: "ETAPAS DEL PROCESO DE OUTSOURCING > P2",
+          url: "/temarioViews/TemaTreitaView",
+          icon: "icon-tag",
+        },
+        {
+          name: "Capacitación",
+          url: "/temarioViews/TemaTreitayunoView",
+          icon: "icon-tag",
+        },
+        {
+          name: "Calidad ",
+          url: "/temarioViews/TemaTreitaydosView",
+          icon: "icon-tag",
+        },
+        {
+          name: "Business Intelligence",
+          url: "/temarioViews/TemaTreitaytresView",
+          icon: "icon-tag",
+        },
+      ],
+
+    },
+
+    
+    
+    
     
   ],
 };
